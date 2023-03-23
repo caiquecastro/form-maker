c9e7c9b14ba0fb3858d15a884a29606c184ea9e7
Caique de Castro <caique.castro@venturus.org.br>
2023-03-23 13:42:15 -0300

    feat: add endpoint to add form question

---
+++ app/routes/forms/$id/questions/index.tsx
@@ -0,0 +1,16 @@
+import { QuestionType } from "@prisma/client";
+import type { ActionArgs } from "@remix-run/node";
+import { redirect } from "react-router";
+import { db } from "~/utils/db.server";
+
+export async function action({ request, params }: ActionArgs) {
+  const body = await request.formData();
+  await db.question.create({
+    data: {
+      title: body.get("title") as string,
+      formId: params.id as string,
+      type: body.get("type") as QuestionType,
+    },
+  });
+  return redirect(`/forms/${params.id}`);
+}
