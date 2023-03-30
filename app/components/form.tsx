fb8f276380dcf8053d4ebee0deb0c1fe57f03590
Caique de Castro <caique.castro@venturus.org.br>
2023-03-30 09:55:54 -0300

    feat: add custom input component

--- app/components/form.tsx
+++ app/components/form.tsx
@@ -1,4 +1,4 @@
-import { SelectHTMLAttributes } from "react";
+import { InputHTMLAttributes, SelectHTMLAttributes } from "react";
 
 type SelectProps = SelectHTMLAttributes<HTMLSelectElement> & {
   label?: string;
@@ -28,5 +28,26 @@ export const Select: React.FC<SelectProps> = ({
         {children}
       </select>
     </div>
+  );
+};
+
+type InputProps = InputHTMLAttributes<HTMLInputElement> & {
+  label?: string;
+};
+
+export const Input: React.FC<InputProps> = ({
+  label,
+  type = "text",
+  ...props
+}) => {
+  return (
+    <>
+      {label ? <label>{label}</label> : null}
+      <input
+        type={type}
+        className="mt-1 w-full rounded-md border-gray-200 shadow-sm sm:text-sm"
+        {...props}
+      />
+    </>
   );
 };
